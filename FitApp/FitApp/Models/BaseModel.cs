3e219be12f6469e6e2f00a460914c8a1263c5a5b
kjszywala <kjszywala@protonmail.com>
2023-06-05 18:17:33 +0100

    Changed project to .net 6

---
+++ FitApp/FitApp/Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FitApp.Models
+{
+    public class BaseModel
+    {
+        public string? Title { get; set; }
+        public string? Notes { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}

fdd2e481abc03239085d4e8a96dbe57f02a335cc
Kvaan <lysymcr@gmail.com>
2023-06-06 19:47:48 +0200

    Add LoginPage

---
+++ FitApp/FitApp/ViewModels/MealsViewModel/MealsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using FitApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FitApp.ViewModels.MealsViewModel
+{
+    public class MealsViewModel
+    {
+        public Command MealsCommand { get; }
+
+        public MealsViewModel()
+        {
+            MealsCommand = new Command(OnMealsClicked);
+        }
+
+        private async void OnMealsClicked(object obj)
+        {
+            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+        }
+    }
+}
+
